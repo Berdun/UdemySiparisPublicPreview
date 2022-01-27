04141a04f5195f336570c8dd35d90d009840e2a0
Baha <Baha@DESKTOP-PD7J2CT>
2022-01-27 17:16:21 +0300

    Add project files.

---
+++ UdemySiparis/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using UdemySiparis.Data.Repository.IRepository;
+using UdemySiparis.Models.ViewModels;
+
+namespace UdemySiparis.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public OrderVM OrderVM { get; set; }
+
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            var orderList = _unitOfWork.OrderProduct.GetAll(x=>x.OrderStatus != "Delivered");
+            return View(orderList);
+        }
+
+
+        public IActionResult Details(int orderId)
+        {
+            OrderVM = new OrderVM
+            {
+                OrderProduct = _unitOfWork.OrderProduct.GetFirstOrDefault(o => o.Id == orderId, includeProperties: "AppUser"),
+                OrderDetails = _unitOfWork.OrderDetails.GetAll(d => d.OrderProductId == orderId, includeProperties: "Product")
+            };
+            return View(OrderVM);
+        }
+
+
+    }
+}
+
+
+//v97-2-2
---
+++ UdemySiparis/Areas/Customer/Controllers/OrderController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using UdemySiparis.Data.Repository.IRepository;
+using UdemySiparis.Models;
+
+namespace UdemySiparis.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    [Authorize]
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<OrderProduct> orderProduct;
+
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+
+            orderProduct = _unitOfWork.OrderProduct.GetAll(u=>u.AppUserId == claim.Value);
+
+            return View(orderProduct);
+        }
+    }
+}
